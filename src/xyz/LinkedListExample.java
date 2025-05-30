4ccf2bcd4186c8fbc7cc31c399ff90caf8335efd
VISHNU-7013 <svishnuvardhan460@gmail.com>
2025-05-30 16:07:38 +0530

    Add files via upload

---
+++ src/xyz/LinkedListExample.java
@@ -0,0 +1,16 @@
+package xyz;
+
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		LinkedList<String> student = new LinkedList<String>();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter names Of 5 students");
+		for(i=1;i<5;i++) {
+			
+		}
+	}
+
+}
