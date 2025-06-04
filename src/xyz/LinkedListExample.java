de54d7ef6dc971498f20ab108ffcae1e3738d05d
VISHNU-7013 <svishnuvardhan460@gmail.com>
2025-06-04 22:17:12 +0530

    Update LinkedListExample.java

--- src/xyz/LinkedListExample.java
+++ src/xyz/LinkedListExample.java
@@ -9,8 +9,15 @@ public class LinkedListExample {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter names Of 5 students");
 		for(i=1;i<5;i++) {
-			
+			System.out.println("Enter name of the student:"+ i +":");
+			String name = sc.nextLine();
+			students.add(name);
 		}
+		System.out.println("students names are:");
+		for(String student : students) {
+			System.out.println(student);
+		}
+		sc.close();
 	}
 
 }
